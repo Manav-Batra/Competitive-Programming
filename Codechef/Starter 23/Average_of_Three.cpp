d81e090f58f47ca7dbb6e9ad519d78f8d9730772
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-01-28 11:51:04 +0530

    Create Average_of_Three.cpp

---
+++ Codechef/Starter 23/Average_of_Three.cpp
@@ -0,0 +1,34 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin>>x;
+        cout<<1<<" "<<3<<" ";
+        cout<<(3*x-4)<<endl;
+    }
+}
