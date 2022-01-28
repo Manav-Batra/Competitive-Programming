34167ae677459a4cdf32095811028d8d90e741ce
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-01-28 11:50:54 +0530

    Create Chef_and_Profits.cpp

---
+++ Codechef/Starter 23/Chef_and_Profits.cpp
@@ -0,0 +1,35 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+        int ans=z-y;
+        ans=ans*x;
+        cout<<ans<<endl;
+    }
+}
