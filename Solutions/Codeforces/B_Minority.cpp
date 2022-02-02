98cbe33cd6f8a47c95bebfd5d7c1c6219498bd59
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-02-02 14:57:24 +0530

    Create B_Minority.cpp

---
+++ Solutions/Codeforces/B_Minority.cpp
@@ -0,0 +1,48 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin>>s;
+        int count1=0,count2=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='1')
+            count1++;
+            else
+            count2++;
+        }
+        if(count1>count2&&count2!=0)
+        cout<<count2<<endl;
+        else if(count1<count2&&count1!=0)
+        cout<<count1<<endl;
+        else if(count1==count2)
+        cout<<count1-1<<endl;
+        else
+        cout<<0<<endl;
+    }
+}
