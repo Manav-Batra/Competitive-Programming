0a6f51f00ce9770dc2aa8f2415256d04fbaa2feb
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-02-16 12:51:48 +0530

    Create height_and_depth_in_trees.cpp

---
+++ Algorithm/graph and trees/height_and_depth_in_trees.cpp
@@ -0,0 +1,52 @@
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+typedef  long long int ll;
+#include<bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v,int n)
+{
+int low=0,high=v.size()-1;
+while(low<=high)
+{
+int mid=(low+high)/2;
+if(v[mid]==n)
+return true;
+else if(v[mid]>n)
+high=mid-1;
+else
+low=mid+1;
+}
+return false;
+}
+const int N=1e5;
+vector<int> g[N];
+int depth[N],height[N];
+void dfs(int vertex,int par=0)
+{
+// cout<<vertex;
+// vis[vertex]=true;
+
+for(auto child : g[vertex])
+{
+if(child==par)
+continue;
+depth[child]=depth[vertex]+1;
+dfs(child,vertex);
+height[vertex]=max(height[vertex],height[child]+1);
+
+}
+}
+int main()
+{
+ int n;
+ cin>>n;
+ for(int i=1;i<=n-1;i++)
+ {
+     int x,y;
+     cin>>x>>y;
+     g[x].push_back(y);
+     g[y].push_back(x);
+ }
+ dfs(1);
+ for(int i=1;i<=n;i++)
+ cout<<depth[i]<<" "<<height[i]<<endl;
+}
