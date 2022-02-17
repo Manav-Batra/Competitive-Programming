4007039c9367f46367cd595f20edf14b94537cbc
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-02-17 19:39:10 +0530

    Create Count_the_Notebooks.cpp

---
+++ Codechef/starter 26/Count_the_Notebooks.cpp
@@ -0,0 +1,45 @@
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+typedef  long long int ll;
+#include<bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v,int n)
+{
+int low=0,high=v.size()-1;
+while(low<=high)
+{
+int mid=(low+high)/2;
+if(v[mid]==n)
+return true;
+else if(v[mid]>n)
+high=mid-1;
+else
+low=mid+1;
+}
+return false;
+}
+const int N=1e5;
+vector<int> g[N];
+bool vis[N];
+void dfs(int vertex)
+{
+cout<<vertex;
+vis[vertex]=true;
+for(auto child : g[vertex])
+{
+if(vis[child])
+continue;
+dfs(child);
+}
+}
+int main()
+{
+ int t;
+cin>>t;
+while(t--)
+{
+    int n;
+    cin>>n;
+    cout<<n*10<<endl;
+    
+}
+}
