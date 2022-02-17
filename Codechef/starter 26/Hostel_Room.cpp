61988df9d93093d90a1d1c80dd6cf9b6e5587316
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-02-17 19:39:16 +0530

    Create Hostel_Room.cpp

---
+++ Codechef/starter 26/Hostel_Room.cpp
@@ -0,0 +1,56 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e5;
+vector<int> g[N];
+bool vis[N];
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int sum=0;
+        int ans=0;
+        vector<int> v(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
+            sum=sum+v[i];
+            ans=max(sum,ans);
+        }
+        cout<<ans+x<<endl;
+    }
+}
