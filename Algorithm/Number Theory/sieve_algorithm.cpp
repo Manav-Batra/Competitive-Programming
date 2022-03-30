982ee8d677dd176f44502cbcfc818170f2c126ce
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-03-30 19:30:34 +0530

    Create sieve_algorithm.cpp

---
+++ Algorithm/Number Theory/sieve_algorithm.cpp
@@ -0,0 +1,81 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e7+10;
+vector<int> g[N];
+bool vis[N];
+int level[N];
+void bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr_vertex = q.front();
+        q.pop();
+        cout << curr_vertex;
+        for (auto child : g[curr_vertex])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[curr_vertex] + 1;
+            }
+        }
+    }
+    cout << endl;
+}
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+// const int N=1e7+10;
+vector<bool> isprime(N,true);
+int main()
+{ 
+    isprime[0]=isprime[1]=false;
+    for(int i=2;i<N;i++)
+    {
+        if(isprime[i]==true)
+        {
+            for(int j=2*i;j<N;j=j+i)
+            {
+                isprime[j]=false;
+            }
+        }
+    }
+    for(int i=1;i<=30;i++)
+    {
+        if(isprime[i])
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
