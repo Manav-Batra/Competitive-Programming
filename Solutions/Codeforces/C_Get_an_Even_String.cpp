72be519a61e730fc53ae85e7c46d7e72deef3dde
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-04-01 19:10:53 +0530

    Create C_Get_an_Even_String.cpp

---
+++ Solutions/Codeforces/C_Get_an_Even_String.cpp
@@ -0,0 +1,83 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e5;
+vector<int> g[N];
+bool vis[N];
+int level[N];
+void bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr_vertex = q.front();
+        q.pop();
+        cout << curr_vertex;
+        for (auto child : g[curr_vertex])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[curr_vertex] + 1;
+            }
+        }
+    }
+    cout << endl;
+}
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin>>s;
+        unordered_map<char,int> m;
+        int ans=0;
+        for(int i=0;i<s.length();i++)
+        {
+            m[s[i]]++;
+            if(m[s[i]]==2)
+            {
+                ans=ans-2;
+                m.clear();
+            }
+            ans++;
+            
+        }
+        cout<<ans<<endl;
+    }
+}
