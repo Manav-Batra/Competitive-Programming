376ee51b25c409a22cdd760396389852cfe65863
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-04-01 10:45:14 +0530

    Create B_Vlad_and_Candies.cpp

---
+++ Solutions/Codeforces/B_Vlad_and_Candies.cpp
@@ -0,0 +1,97 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e5;
+vector<int> g[N];
+bool vis[N];
+int level[N];
+void bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr_vertex = q.front();
+        q.pop();
+        cout << curr_vertex;
+        for (auto child : g[curr_vertex])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[curr_vertex] + 1;
+            }
+        }
+    }
+    cout << endl;
+}
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin>>n;
+        vector<ll> a(n);
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        if(n==1&&a[0]!=1)
+        {
+            cout<<"NO"<<endl;
+            continue;
+        }
+        if(n==1&&a[0]==1)
+        {
+            cout<<"YES"<<endl;
+            continue;
+        }
+        sort(a.begin(),a.end());
+        ll max=a[n-1];
+        ll min=a[n-2];
+        // for(int i=1;i<n-1;i++)
+        // {
+        //     if(a[i]>min&&a[i]<=max)
+        //     min=a[i];
+        // }
+        if((max-min)>1)
+        {
+            cout<<"NO"<<endl;
+            continue;
+        }
+        cout<<"YES"<<endl;
+        
+    }
+}
