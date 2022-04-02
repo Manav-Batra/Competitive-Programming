634f2ad94f2417489f7ee4e0bde1c09ec9286e19
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-04-02 19:44:41 +0530

    Create C_Given_Length_and_Sum_of_Digits.cpp

---
+++ Solutions/Codeforces/C_Given_Length_and_Sum_of_Digits.cpp
@@ -0,0 +1,149 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e5;
+vector<int> g[N];
+bool vis[N];
+int level[N];
+void bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr_vertex = q.front();
+        q.pop();
+        cout << curr_vertex;
+        for (auto child : g[curr_vertex])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[curr_vertex] + 1;
+            }
+        }
+    }
+    cout << endl;
+}
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+int main()
+{
+    ll m, s;
+    cin >> m >> s;
+    if (m == 1 && (s >= 0 && s <= 9))
+    {
+        cout << s << " " << s << endl;
+        return 0;
+    }
+    ll check = 9 * m;
+    if (m == 1 && s > check)
+    {
+        cout << -1 << " " << -1 << endl;
+        return 0;
+    }
+    if (m != 1 && (s > check || s == 0))
+    {
+        cout << -1 << " " << -1 << endl;
+        return 0;
+    }
+    else
+    {
+        vector<ll> min(m);
+        ll n=s;
+        ll temp=m-1;
+        for(;temp>=0;temp--)
+        {
+            if(n>9)
+            {
+                min[temp]=9;
+                n=n-9;
+            }
+            
+            else
+            {
+                min[temp]=n-1;
+                break;
+            }
+        }
+        ll sum=0;
+        for(int i=1;i<m;i++)
+        sum+=min[i];
+        sum=s-sum;
+        min[0]=sum;
+
+        // for (auto it : min)
+        // {
+        //     cout << it;
+        // }
+        // cout << " ";
+        vector<ll> max;
+        n = s;
+        if (s < 10)
+        {
+            max.push_back(s);
+            n = 0;
+        }
+        else
+        {
+            max.push_back(9);
+            n = n - 9;
+        }
+        temp = m - 1;
+        while (temp--)
+        {
+            if (n >= 9)
+            {
+                max.push_back(9);
+                n = n - 9;
+            }
+            else
+            {
+                max.push_back(n);
+                n = 0;
+            }
+        }
+        // for (auto it : max)
+        // {
+        //     cout << it;
+        // }
+        // cout << endl;
+        for(auto it : min)
+        cout<<it;
+        cout<<" ";
+        for(auto it : max)
+        cout<<it;
+        cout<<endl;
+        
+    }
+}
