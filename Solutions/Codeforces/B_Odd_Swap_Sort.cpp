fa24dccea329007bb028b0808eb49eabe26f23d0
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-04-08 17:52:29 +0530

    Create B_Odd_Swap_Sort.cpp

---
+++ Solutions/Codeforces/B_Odd_Swap_Sort.cpp
@@ -0,0 +1,94 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e5;
+vector<int> g[N];
+bool vis[N];
+int level[N];
+void bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr_vertex = q.front();
+        q.pop();
+        cout << curr_vertex;
+        for (auto child : g[curr_vertex])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[curr_vertex] + 1;
+            }
+        }
+    }
+    cout << endl;
+}
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin>>n;
+        vector<ll> v(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
+        }
+        vector<ll> v1,v2,odd,even;
+        for(auto it : v)
+        {
+            if(it&1)
+            {
+                v1.push_back(it);
+                odd.push_back(it);
+            }
+            else
+            {
+                v2.push_back(it);
+                even.push_back(it);
+            }
+        }
+        sort(v1.begin(),v1.end());
+        sort(v2.begin(),v2.end());
+        if(v1==odd&&v2==even)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+}
