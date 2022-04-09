fa9222b04fed31bd7519a533b379913e2d5bb64f
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-04-09 10:50:57 +0530

    Create C_Make_Equal_With_Mod.cpp

---
+++ Solutions/Codeforces/C_Make_Equal_With_Mod.cpp
@@ -0,0 +1,105 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<ll> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e5;
+vector<int> g[N];
+bool vis[N];
+int level[N];
+void bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr_vertex = q.front();
+        q.pop();
+        cout << curr_vertex;
+        for (auto child : g[curr_vertex])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[curr_vertex] + 1;
+            }
+        }
+    }
+    cout << endl;
+}
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin>>n;
+        vector<ll> v(n);
+        set<ll> s;
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
+            s.insert(v[i]);
+        }
+        if(s.size()==1)
+        {
+            cout<<"YES"<<endl;
+            continue;
+        }
+        sort(v.begin(),v.end());
+        if(bs(v,1))
+        {
+            bool ok=false;
+            for(int i=0;i<n-1;i++)
+            {
+                if((v[i]+1)==v[i+1])
+                {
+                    ok=true;
+                    break;
+                }
+            }
+            if(ok)
+            {
+                cout<<"NO"<<endl;
+
+            }
+            else
+            {
+               cout<<"YES"<<endl;
+            }
+        }
+        else
+        cout<<"YES"<<endl;
+    }
+}
