2e863724e02dd735a3c73386d3489eda0dc544a0
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-05-19 00:09:56 +0530

    Create Closest_Vowels.cpp

---
+++ Codechef/starter 39/Closest_Vowels.cpp
@@ -0,0 +1,90 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e5;
+vector<int> g[N];
+bool vis[N];
+int level[N];
+void bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr_vertex = q.front();
+        q.pop();
+        cout << curr_vertex;
+        for (auto child : g[curr_vertex])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[curr_vertex] + 1;
+            }
+        }
+    }
+    cout << endl;
+}
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+long long mod=1e9+7;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='c'||s[i]=='g'||s[i]=='l'||s[i]=='r')
+            {
+                ans++;
+            }
+        }
+        
+        // cout<<ans<<endl;
+        long long res=1;
+        while(ans--)
+        {
+            res=(res*2)%mod;
+        }
+        res=res%mod;
+        cout<<res<<endl;
+
+    }
+}
