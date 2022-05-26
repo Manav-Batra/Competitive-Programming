f82adf349bbbf9a31cd0c94c4ad1f8558bee25e8
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-05-26 14:21:46 +0530

    Create find_the_median.cpp

---
+++ gfg/find_the_median.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+public:
+	public:
+		int find_median(vector<int> v)
+		{
+		    // Code here.
+		    int ans;
+		    sort(v.begin(),v.end());
+		    int n=v.size();
+		    if(n&1)
+		    {
+		        int m=n/2;
+		        ans=v[m];
+		    }
+		    else
+		    {
+		        int m=n/2-1;
+		        int t=m+1;
+		        ans=(v[m]+v[t])/2;
+		    }
+		    return ans;
+		}
+};
+
+// { Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>v[i];
+    	Solution ob;
+    	int ans = ob.find_median(v);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+  // } Driver Code Ends
