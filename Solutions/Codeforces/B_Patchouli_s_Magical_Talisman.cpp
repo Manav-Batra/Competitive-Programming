42c84c79d760b91de845244e72a5c5b913e1f6f5
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-06-04 00:00:11 +0530

    Create B_Patchouli_s_Magical_Talisman.cpp

---
+++ Solutions/Codeforces/B_Patchouli_s_Magical_Talisman.cpp
@@ -0,0 +1,119 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e5;
+vector<int> g[N];
+bool vis[N];
+int level[N];
+void bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr_vertex = q.front();
+        q.pop();
+        cout << curr_vertex;
+        for (auto child : g[curr_vertex])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[curr_vertex] + 1;
+            }
+        }
+    }
+    cout << endl;
+}
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+        bool ok = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i] % 2 == 0)
+            {
+                ok = false;
+                break;
+            }
+        }
+        if (ok == true)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+        ok = false;
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i] % 2 != 0)
+            {
+                ok = true;
+                break;
+            }
+        }
+        if (ok == true)
+        {
+            ll count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (v[i] % 2 == 0)
+                    count++;
+            }
+            cout << count << endl;
+            continue;
+        }
+        ll ans = 0;
+        ll mini=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            while((v[i]%2==0))
+            {
+                ans++;
+                v[i]=v[i]/2;
+            }
+            mini=min(ans,mini);
+            ans=0;
+        }
+        cout<<mini+n-1<<endl;
+    }
+}
