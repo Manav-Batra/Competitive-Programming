9c02aa6542b216133316d172ed78aefeaf47f27d
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-06-09 16:38:13 +0530

    Create E_Price_Maximization.cpp

---
+++ Solutions/Codeforces/E_Price_Maximization.cpp
@@ -0,0 +1,104 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e5;
+vector<int> g[N];
+bool vis[N];
+int level[N];
+void bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr_vertex = q.front();
+        q.pop();
+        cout << curr_vertex;
+        for (auto child : g[curr_vertex])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[curr_vertex] + 1;
+            }
+        }
+    }
+    cout << endl;
+}
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n,k;
+        cin>>n>>k;
+        vector<ll> v(n);
+        for(ll i=0;i<n;i++)
+        {
+            cin>>v[i];
+
+        }
+       
+        ll ans=0;
+        for(ll i=0;i<n;i++)
+        {
+            ans+=(v[i]/k);
+
+        }
+        for(ll i=0;i<n;i++)
+        {
+            v[i]=v[i]%k;
+        }
+         sort(v.begin(),v.end());
+        ll i=0;
+        ll j=n-1;
+        while(i<j)
+        {
+            ll temp=((v[i])+(v[j]));
+            if(temp>=k)
+            {
+                j--;
+                i++;
+                ans=ans+1;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        cout<<ans<<endl;
+    }
+}
