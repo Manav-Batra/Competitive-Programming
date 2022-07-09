c54d70e9a8b85d0fafd006b1b36ef7cd8ee4fba2
Manav-Batra <95028933+Manav-Batra@users.noreply.github.com>
2022-07-09 11:36:46 +0530

    Create B_Permutation.cpp

---
+++ Solutions/Codeforces/B_Permutation.cpp
@@ -0,0 +1,86 @@
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+typedef long long int ll;
+#include <bits/stdc++.h>
+using namespace std;
+bool bs(vector<int> v, int n)
+{
+    int low = 0, high = v.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (v[mid] == n)
+            return true;
+        else if (v[mid] > n)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return false;
+}
+const int N = 1e5;
+vector<int> g[N];
+bool vis[N];
+int level[N];
+void bfs(int source)
+{
+    queue<int> q;
+    q.push(source);
+    vis[source] = true;
+    while (!q.empty())
+    {
+        int curr_vertex = q.front();
+        q.pop();
+        cout << curr_vertex;
+        for (auto child : g[curr_vertex])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[curr_vertex] + 1;
+            }
+        }
+    }
+    cout << endl;
+}
+void dfs(int vertex)
+{
+    cout << vertex;
+    vis[vertex] = true;
+    for (auto child : g[vertex])
+    {
+        if (vis[child])
+            continue;
+        dfs(child);
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin>>n;
+        cout<<2<<endl;
+        vector<bool> a(n+1);
+
+        for(int i=1;i<=n;i++)
+        {
+            if(a[i]==true)
+            continue;
+            cout<<i<<" ";
+            int d=i*2;
+            while(d<=n)
+            {
+                a[d]=true;
+               cout<<d<<" ";
+               d=d*2;
+
+            }
+        }
+    }
+}
